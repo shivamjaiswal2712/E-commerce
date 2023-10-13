3123a5e5c966ed86605cd0a7c113570074098959
shivamjaiswal127 <shivamjaiswal271299@gmail.com>
2023-10-13 20:02:12 +0530

    add files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+      <>
+        <footer className="mb-0 text-center">
+            <div className="d-flex align-items-center justify-content-center pb-5">
+                <div className="col-md-6">
+                    <p className="mb-3 mb-md-0">Made with by {" "}
+                        <a href="https://shivamjaiswal" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Shivam Jaiswal</a>
+                    </p>
+                    <a className="text-dark fs-4" href="https://github.com/shivamjaiswal2712" target="_blank" rel="noreferrer">
+                        <i className="fa fa-github" />
+                    </a>
+                </div>
+            </div>
+        </footer>
+      </>
+    );
+}
+
+export default Footer;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Footer from "../components/Footer"
+
+const Home = () => {
+    return(
+        <>
+            <Footer />
+        </>
+    );
+}
+
+export default Home;
