c2c56a4ca9a63820f9a1220e9d9b83dda4920d3c
shivamjaiswal127 <shivamjaiswal271299@gmail.com>
2023-11-03 19:59:21 +0530

    update files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light py-3 sticky-top">
             <div className="container">
-                <NavLink className="navbar-brand fw-bold fs-4 px-2" to="/"><span className="">ShopAt</span></NavLink>
+                <NavLink className="navbar-brand fw-bold fs-4 px-2" to="/"><span className="text-danger">ShopAt</span></NavLink>
                 <button className="navbar-toggler mx-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon" />
                 </button>
@@ -19,20 +19,20 @@ const Navbar = () => {
                             <NavLink className="nav-link" to="/">Home</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink className="nav-link" to="product">Products</NavLink>
+                            <NavLink className="nav-link" to="/product">Products</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink className="nav-link" to="about">About</NavLink>
+                            <NavLink className="nav-link" to="/about">About</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink className="nav-link" to="contact">Contact</NavLink>
+                            <NavLink className="nav-link" to="/contact">Contact</NavLink>
                         </li>
                     </ul>
 
                     <div className="buttons text-center">
                         <NavLink to="/login" className="btn btn-outline-dark m-2"><i className="fa fa-sign-in-alt mr-1" /> Login</NavLink>
                         <NavLink to="/register" className="btn btn-outline-dark m-2"><i className="fa fa-user-plus mr-1" /> Register</NavLink>
-                        <NavLink to="/cart" className="btn btn-outline-dark m-2"><i className="fa fa-cart-shopping mr-1" /> cart ({state.length})</NavLink>
+                        <NavLink to="/cart" className="btn btn-outline-dark m-2"><i className="fa fa-cart-shopping mr-1" /> Cart ({state.length})</NavLink>
                     </div>
                 </div>
             </div>
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -5,12 +5,13 @@ const Homes = () => {
         <>
             <div className="hero border-1 pb-3">
                 <div className="card bg-dark text-white border-0 mx-3">
-                    <img className="card-img img-fluid" src="./assets/shopAt2.jpg" alt="" height={500} />
+                    <img className="card-img img-fluid" src="./assets/back2.jpeg" alt="" height={500} />
                     <div className="card-img-overlay d-flex align-items-center">
                         <div className="container">
                             <h5 className="card-title fs-1 text fw-lighter">New Season Arrivals</h5>
-                            <p className="card-text fs-5 d-none d-sm-block">
-                                This is a wider card with supporting text below asa natural lead-in to additional content. This content is a little bit longer.
+                            <p className="card-text fs-4 d-none d-sm-block">
+                                This is a wider card with supporting text below as a natural lead-in to additional content.
+                                This content is a little bit longer.
                             </p>
                         </div>
                     </div>
